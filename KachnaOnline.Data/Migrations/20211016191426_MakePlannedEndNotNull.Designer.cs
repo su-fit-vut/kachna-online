b9c99144a6cc266dc379ebfba3b90c042dd46e4d
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2021-10-16 21:17:25 +0200

    Make PlannedState's PlannedEnd not nullable
    We always want to keep an end date in the database. Null PlannedEnds would only make sense if we saved Closed states to the database and we don't do that.

--- KachnaOnline.Data/Migrations/20211016191426_MakePlannedEndNotNull.Designer.cs
+++ KachnaOnline.Data/Migrations/20211016191426_MakePlannedEndNotNull.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using KachnaOnline.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KachnaOnline.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211016191426_MakePlannedEndNotNull")]
+    partial class MakePlannedEndNotNull
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
