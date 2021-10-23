c5cb56dfd8420cf3482b7b7aeebf8d3503302208
David Chocholatý <chocholaty.david@protonmail.com>
2021-10-24 00:42:11 +0200

    Add event constant for maximal query time span

---
+++ KachnaOnline.Business/Constants/EventConstants.cs
@@ -0,0 +1,11 @@
+// EventConstants.cs
+// Author: David Chocholatý
+
+namespace KachnaOnline.Business.Constants
+{
+    public static class EventConstants
+    {
+        // Default time span to query for in days.
+        public const int QueryDaysTimeSpan = 180;
+    }
+}
