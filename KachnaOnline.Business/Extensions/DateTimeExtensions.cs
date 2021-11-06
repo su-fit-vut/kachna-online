eb97e3c21dbce8b658d2f36ae2b62b2d0bd8a234
David Chocholatý <chocholaty.david@protonmail.com>
2021-11-06 18:22:40 +0100

    Round DateTime to minutes

---
+++ KachnaOnline.Business/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+// DateTimeExtensions.cs
+// Author: Ondřej Ondryáš
+
+using System;
+
+namespace KachnaOnline.Business.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Creates a new <see cref="DateTime"/> that only keeps the date, hour and minute components
+        /// from the original <paramref name="dateTime"/>.
+        /// </summary>
+        /// <param name="dateTime">The <see cref="DateTime"/> to round.</param>
+        /// <returns>A <see cref="DateTime"/> structure with the date, hour and minute components set.</returns>
+        public static DateTime RoundToMinutes(this DateTime dateTime)
+        {
+            return new DateTime(dateTime.Year, dateTime.Month, dateTime.Day,
+                dateTime.Hour, dateTime.Minute, 0);
+        }
+    }
+}
