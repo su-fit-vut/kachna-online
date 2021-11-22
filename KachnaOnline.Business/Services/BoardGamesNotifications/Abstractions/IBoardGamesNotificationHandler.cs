631c29abce21914ad33e799eccf1ae6a1015e1f0
František Nečas <frantisek.necas@protonmail.com>
2021-11-22 08:15:44 +0100

    Prepare necessary abstractions for notifications
    Signed-off-by: František Nečas <frantisek.necas@protonmail.com>

---
+++ KachnaOnline.Business/Services/BoardGamesNotifications/Abstractions/IBoardGamesNotificationHandler.cs
@@ -0,0 +1,41 @@
+// IBoardGamesNotificationHandler.cs
+// Author: František Nečas
+
+using System.Threading.Tasks;
+
+namespace KachnaOnline.Business.Services.BoardGamesNotifications.Abstractions
+{
+    /// <summary>
+    /// Represents a single type of action that happens when a board game stata changes or is about to change.
+    /// </summary>
+    public interface IBoardGamesNotificationHandler
+    {
+        /// <summary>
+        /// Performs this handler's action for reservation creation.
+        /// </summary>
+        /// <param name="reservationId">ID of the newly created reservation.</param>
+        /// <returns><see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task PerformReservationCreated(int reservationId);
+
+        /// <summary>
+        /// Performs this handler's action for request of reservation extension.
+        /// </summary>
+        /// <param name="itemId">ID of the reservation item where extension was requested.</param>
+        /// <returns><see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task PerformReservationItemExtensionRequest(int itemId);
+
+        /// <summary>
+        /// Performs this handler's for a reservation item expiring soon (e.g. in 3 days).
+        /// </summary>
+        /// <param name="itemId">ID of the reservation item that is about to expire.</param>
+        /// <returns><see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task PerformReservationItemExpiresSoon(int itemId);
+
+        /// <summary>
+        /// Performs this handler's action for a reservation item expiration.
+        /// </summary>
+        /// <param name="itemId">ID of the reservation item that just expired.</param>
+        /// <returns><see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task PerformReservationItemExpired(int itemId);
+    }
+}
---
+++ KachnaOnline.Business/Services/BoardGamesNotifications/Abstractions/IBoardGamesNotificationService.cs
@@ -0,0 +1,42 @@
+// IBoardGamesNotificationService.cs
+// Author: František Nečas
+
+using System.Threading.Tasks;
+
+namespace KachnaOnline.Business.Services.BoardGamesNotifications.Abstractions
+{
+    /// <summary>
+    /// Represents a service that performs actions when the state of a board games reservations changes
+    /// or is about to change. This includes sending emails, Discord messages etc.
+    /// </summary>
+    public interface IBoardGamesNotificationService
+    {
+        /// <summary>
+        /// Performs actions for reservation creation.
+        /// </summary>
+        /// <param name="reservationId">ID of the newly created reservation.</param>
+        /// <returns><see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task TriggerReservationCreated(int reservationId);
+
+        /// <summary>
+        /// Performs actions for request of reservation extension.
+        /// </summary>
+        /// <param name="itemId">ID of the reservation item where extension was requested.</param>
+        /// <returns><see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task TriggerReservationItemExtensionRequest(int itemId);
+
+        /// <summary>
+        /// Performs actions for a reservation item expiring soon (e.g. in 3 days).
+        /// </summary>
+        /// <param name="itemId">ID of the reservation item that is about to expire.</param>
+        /// <returns><see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task TriggerReservationItemExpiresSoon(int itemId);
+
+        /// <summary>
+        /// Performs actions for a reservation item expiration.
+        /// </summary>
+        /// <param name="itemId">ID of the reservation item that just expired.</param>
+        /// <returns><see cref="Task"/> that represents the asynchronous operation.</returns>
+        Task TriggerReservationItemExpired(int itemId);
+    }
+}
