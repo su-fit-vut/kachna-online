0422b84910a061f9a68488cfa8c80a850724ffd8
František Nečas <frantisek.necas@protonmail.com>
2021-11-22 10:39:43 +0100

    Add webhook message ID to reservations
    Signed-off-by: František Nečas <frantisek.necas@protonmail.com>

---
+++ KachnaOnline.Data/Migrations/20211122093931_AddWebhookMessageId.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KachnaOnline.Data.Migrations
+{
+    public partial class AddWebhookMessageId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "WebhookMessageId",
+                table: "BoardGameReservations",
+                type: "numeric(20,0)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WebhookMessageId",
+                table: "BoardGameReservations");
+        }
+    }
+}
