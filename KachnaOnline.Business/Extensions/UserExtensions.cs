d0903bea0f0901b757ff923ccfc8c42698bf60b5
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2021-11-26 00:12:44 +0100

    Add SU Discord state transition handler

---
+++ KachnaOnline.Business/Extensions/UserExtensions.cs
@@ -0,0 +1,21 @@
+// UserExtensions.cs
+// Author: Ondřej Ondryáš
+
+using KachnaOnline.Business.Models.Users;
+
+namespace KachnaOnline.Business.Extensions
+{
+    public static class UserExtensions
+    {
+        public static string GetDiscordMention(this User user, bool showName = false)
+        {
+            if (user is null)
+                return null;
+
+            if (!user.DiscordId.HasValue)
+                return user.Nickname ?? (showName ? user.Name : null);
+
+            return $"<@{user.DiscordId.Value}>";
+        }
+    }
+}
