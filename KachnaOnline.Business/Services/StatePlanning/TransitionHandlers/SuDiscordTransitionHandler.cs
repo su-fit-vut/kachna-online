e356c3f913576fc72e6d21082bea4025f3d60927
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2021-11-26 00:12:44 +0100

    Add SU Discord state transition handler

--- KachnaOnline.Business/Services/StatePlanning/TransitionHandlers/SuDiscordTransitionHandler.cs
+++ KachnaOnline.Business/Services/StatePlanning/TransitionHandlers/SuDiscordTransitionHandler.cs
@@ -1,6 +1,9 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using KachnaOnline.Business.Configuration;
+using KachnaOnline.Business.Extensions;
+using KachnaOnline.Business.Models.ClubStates;
 using KachnaOnline.Business.Services.Abstractions;
 using KachnaOnline.Business.Services.Discord;
 using KachnaOnline.Business.Services.StatePlanning.Abstractions;
@@ -13,24 +16,93 @@ namespace KachnaOnline.Business.Services.StatePlanning.TransitionHandlers
     {
         private readonly ILogger<SuDiscordTransitionHandler> _logger;
         private readonly IClubStateService _stateService;
+        private readonly IUserService _userService;
 
         public SuDiscordTransitionHandler(IOptionsMonitor<ClubStateOptions> options,
             IHttpClientFactory httpClientFactory,
-            ILogger<SuDiscordTransitionHandler> logger, IClubStateService stateService)
+            ILogger<SuDiscordTransitionHandler> logger, IClubStateService stateService,
+            IUserService userService)
             : base(options.CurrentValue.SuDiscordWebhookUrl, httpClientFactory, logger)
         {
             _logger = logger;
             _stateService = stateService;
+            _userService = userService;
         }
 
-        public Task PerformStartAction(int stateId, int? previousStateId)
+        public async Task PerformStartAction(int stateId, int? previousStateId)
         {
-            throw new System.NotImplementedException();
+            var state = await _stateService.GetState(stateId);
+            var previousState = previousStateId.HasValue ? await _stateService.GetState(previousStateId.Value) : null;
+
+            var madeBy = state.MadeById.HasValue ? await _userService.GetUser(state.MadeById.Value) : null;
+            var madeByName = madeBy.GetDiscordMention();
+
+            string msg = null;
+
+            if (state.Type == StateType.OpenChillzone)
+            {
+                if (!state.PlannedEnd.HasValue)
+                {
+                    _logger.LogCritical("Data inconsistency: chillzone with no planned end (ID {Id}).", state.Id);
+                    return;
+                }
+
+                if (previousState?.Type == StateType.OpenChillzone)
+                {
+                    if (previousState.MadeById != state.MadeById)
+                    {
+                        msg = $"Předáno {madeByName} v {state.Start:HH:mm}. ";
+                    }
+
+                    if (previousState.PlannedEnd.HasValue)
+                    {
+                        var timeDelta = previousState.PlannedEnd.Value - state.PlannedEnd.Value;
+
+                        if (timeDelta > TimeSpan.Zero)
+                        {
+                            msg += $"Zkráceno do {state.PlannedEnd.Value:HH:mm}.";
+                        }
+                        else if (timeDelta < TimeSpan.Zero)
+                        {
+                            msg += $"Prodlouženo do {state.PlannedEnd.Value:HH:mm}.";
+                        }
+                    }
+                }
+                else
+                {
+                    msg = $"Otevřeno od {state.Start:HH:mm} do {state.PlannedEnd.Value:HH:mm}. Otevírá {madeByName}.";
+                }
+            }
+            else if (state.Type == StateType.OpenBar && previousState?.Type == StateType.OpenChillzone)
+            {
+                msg = "Ukončeno otvíračkou.";
+            }
+
+            if (msg != null)
+            {
+                if (state.NoteInternal != null)
+                {
+                    msg += "\\nInterní poznámka: " + state.NoteInternal;
+                }
+
+                if (state.NotePublic != null)
+                {
+                    msg += "\\nVeřejná poznámka: " + state.NotePublic;
+                }
+
+                _logger.LogDebug("Sending chillzone start message to SU Discord server.");
+                await this.SendWebhookMessage(msg);
+            }
         }
 
-        public Task PerformEndAction(int stateId, int? nextStateId)
+        public async Task PerformEndAction(int stateId, int? nextStateId)
         {
-            throw new System.NotImplementedException();
+            var state = await _stateService.GetState(stateId);
+            if (state.Type == StateType.OpenChillzone && !nextStateId.HasValue)
+            {
+                _logger.LogDebug("Sending chillzone end message to SU Discord server.");
+                await this.SendWebhookMessage($"Zavřeno v {DateTime.Now:HH:mm}.");
+            }
         }
     }
 }
