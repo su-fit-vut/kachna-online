f1c40ba0627a05b2251fb193e948f00bcefefff9
František Nečas <frantisek.necas@protonmail.com>
2021-11-27 17:58:22 +0100

    Rename board game card component
    Signed-off-by: František Nečas <frantisek.necas@protonmail.com>

--- ClientApp/KachnaOnline/src/app/board-games/board-games.module.ts
+++ ClientApp/KachnaOnline/src/app/board-games/board-games.module.ts
@@ -8,7 +8,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { BoardGamesRoutingModule } from './board-games-routing.module';
 import { BoardGamesPageComponent } from './board-games-page/board-games-page.component';
-import { BoardGameComponent } from './board-games-page/board-game/board-game.component';
+import { BoardGameCardComponent } from './board-games-page/board-game-card/board-game-card.component';
 import { CategorySelectorComponent } from './board-games-page/category-selector/category-selector.component';
 import { CategoryComponent } from './board-games-page/category-selector/category/category.component';
 import { TogglableButtonComponent } from './board-games-page/togglable-button/togglable-button.component';
@@ -20,7 +20,7 @@ import { ReservationsComponent } from './reservations/reservations.component';
 @NgModule({
   declarations: [
     BoardGamesPageComponent,
-    BoardGameComponent,
+    BoardGameCardComponent,
     CategorySelectorComponent,
     CategoryComponent,
     TogglableButtonComponent,
