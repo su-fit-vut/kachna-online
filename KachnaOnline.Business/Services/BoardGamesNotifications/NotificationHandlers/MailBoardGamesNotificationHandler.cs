4f89533f01983837021e69cb75922b3bee542098
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2021-11-27 18:03:26 +0100

    Do not create custom SMTP credentials if not configured

--- KachnaOnline.Business/Services/BoardGamesNotifications/NotificationHandlers/MailBoardGamesNotificationHandler.cs
+++ KachnaOnline.Business/Services/BoardGamesNotifications/NotificationHandlers/MailBoardGamesNotificationHandler.cs
@@ -72,8 +72,7 @@ namespace KachnaOnline.Business.Services.BoardGamesNotifications.NotificationHan
             // TODO: Cache SMTP clients. Making a new one for each e-mail may lead to starvation.
 
             var config = _smtpOptionsMonitor.CurrentValue;
-            if (string.IsNullOrEmpty(config.FromAddress) || string.IsNullOrEmpty(config.Host) ||
-                string.IsNullOrEmpty(config.Password))
+            if (string.IsNullOrEmpty(config.FromAddress) || string.IsNullOrEmpty(config.Host))
             {
                 _logger.LogError("SMTP authentication not available.");
                 return;
@@ -87,10 +86,14 @@ namespace KachnaOnline.Business.Services.BoardGamesNotifications.NotificationHan
                 Host = config.Host,
                 Port = config.Port,
                 EnableSsl = config.UseSsl,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(config.Username, config.Password)
+                DeliveryMethod = SmtpDeliveryMethod.Network
             };
+
+            if (!string.IsNullOrEmpty(config.Username) || !string.IsNullOrEmpty(config.Password))
+            {
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(config.Username, config.Password);
+            }
 
             var message = new MailMessage(fromAddress, toAddress)
             {
@@ -130,7 +133,7 @@ namespace KachnaOnline.Business.Services.BoardGamesNotifications.NotificationHan
                 var expiration = item.ExpiresOn.Value;
                 // TODO: possibly include a link to frontend card where the game can be extended.
                 var message = $@"Ahoj,<br><br>
-Tvá výpůjčka deskové hry <b>{game.Name}</b> již brzy vyprší, konkrétně 
+Tvá výpůjčka deskové hry <b>{game.Name}</b> již brzy vyprší, konkrétně
 <b>{expiration:dd. MM. yyyy}</b>.
 Domluv se, prosím, s někým ze Studentské unie na vrácení hry zpět do klubu. V případě, že
 se ti hra zalíbila a rád bys ji měl*a půjčenou ještě déle, můžeš na webu Kachna Online požádat
@@ -177,7 +180,7 @@ Tvoje Kachna Online";
 
                 // TODO: possibly include a link to frontend card where the game can be extended.
                 var message = $@"Ahoj,<br><br>
-Tvá výpůjčka deskové hry <b>{game.Name}</b> vypršela. 
+Tvá výpůjčka deskové hry <b>{game.Name}</b> vypršela.
 Domluv se, prosím, s někým ze Studentské unie na vrácení hry zpět do klubu. V případě, že
 se ti hra zalíbila a rád bys ji měl*a půjčenou ještě déle, můžeš na webu Kachna Online požádat
 o prodloužení nebo ti ji může prodloužit člen Studentské unie, pokud se s ním domluvíš.<br><br>
