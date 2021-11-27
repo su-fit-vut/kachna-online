a743088d406522aac56c510dd8ad773ef345fba7
David Chocholatý <chocholaty.david@protonmail.com>
2021-11-27 21:25:53 +0100

    Show event detail when requested

---
+++ ClientApp/KachnaOnline/src/app/events/linked-states/linked-states.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linked-states',
+  templateUrl: './linked-states.component.html',
+  styleUrls: ['./linked-states.component.css']
+})
+export class LinkedStatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
