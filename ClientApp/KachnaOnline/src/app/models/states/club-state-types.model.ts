41606ffd064701de11e049f76af6e4c152bded0d
David Chocholatý <chocholaty.david@protonmail.com>
2021-11-28 13:55:33 +0100

    Add enum for club state types

---
+++ ClientApp/KachnaOnline/src/app/models/states/club-state-types.model.ts
@@ -0,0 +1,9 @@
+// club-state-types.model.ts
+// Author: David Chocholatý
+
+export enum ClubStateTypes {
+  OpenBar = "OpenBar",
+  OpenChillzone = "OpenChillzone",
+  Private = "Private",
+  Closed = "Closed"
+}
