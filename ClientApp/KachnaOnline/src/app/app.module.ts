3989b27066bf77e4493c557778678c447e316684
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2021-11-28 14:07:19 +0100

    Implement home page, make layout adjustments

--- ClientApp/KachnaOnline/src/app/app.module.ts
+++ ClientApp/KachnaOnline/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HttpClientModule, HttpRequest } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EventFormComponent } from './events/event-form/event-form.component';
 import { HomeComponent } from './home/home.component';
+import { ChillzoneDetailsComponent } from './home/chillzone-details/chillzone-details.component';
 import { EventsFromAllComponent } from './events/events-from-all/events-from-all.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
@@ -59,6 +60,7 @@ export function tokenGetter(request?: HttpRequest<any>) {
     LoggedOutContentComponent,
     PlanEventsComponent,
     ForbiddenComponent,
+    ChillzoneDetailsComponent,
   ],
   imports: [
     ComponentsModule,
--- ClientApp/KachnaOnline/src/app/home/home.component.ts
+++ ClientApp/KachnaOnline/src/app/home/home.component.ts
@@ -3,6 +3,9 @@
 
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from "../shared/services/authentication.service";
+import { ClubState } from "../models/states/club-state.model";
+import { ClubStateTypes } from "../models/states/club-state-types.model";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-home',
@@ -11,13 +14,29 @@ import { AuthenticationService } from "../shared/services/authentication.service
 })
 export class HomeComponent implements OnInit {
 
-  constructor(
-    public authenticationService: AuthenticationService,
-  ) { }
+  constructor(public authenticationService: AuthenticationService, public modalService: NgbModal) {
+  }
 
-  jumbotronStateMainText: string = "Kachna je zavřená.";
+  ST = ClubStateTypes;
+  state: ClubState;
 
   ngOnInit(): void {
+    this.state = {
+      id: 1,
+      state: ClubStateTypes.OpenChillzone,
+      madeByUser: {
+        id: 195,
+        name: "LeO"
+      },
+      start: new Date(2021, 11, 28, 10, 11, 1),
+      plannedEnd: new Date(2021, 11, 28, 14, 28, 32),
+      note: "Velmi pěkná veřejná poznámka",
+      noteInternal: "Interní poznámka se zobrazuje jen manažerům",
+      actualEnd: null,
+      closedByUser: null,
+      eventId: null,
+      followingState: null
+    }
   }
 
 }
