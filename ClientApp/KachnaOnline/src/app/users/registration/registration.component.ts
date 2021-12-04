8eb06cfea2469cb1048242e36b62187e5e1d7def
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2021-12-05 00:26:58 +0100

    Redesign the user profile form

--- ClientApp/KachnaOnline/src/app/users/registration/registration.component.ts
+++ ClientApp/KachnaOnline/src/app/users/registration/registration.component.ts
@@ -4,17 +4,26 @@ import { AuthenticationService } from "../../shared/services/authentication.serv
 import { ToastrService } from "ngx-toastr";
 import { ActivatedRoute, Params } from "@angular/router";
 
+const prestigeText = 'Členové klubu jsou ohodnocování virtuálními body, tzv. prestiží, za jejich účast na aktivitách ' +
+  'a akcích SU a za poskytování členských příspěvků. Pro sbírání těchto bodů při nakupování občerstvení v klubu ' +
+  'je nutné udělit souhlas.';
+
+export { prestigeText };
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+
+  prestigeTooltipText: string = prestigeText;
+
   constructor(
     private authenticationService: AuthenticationService,
     private toastr: ToastrService,
     private route: ActivatedRoute,
-  ) { }
+  ) {}
 
   form = new FormGroup({
     sympathizingMemberConsent: new FormControl(false),
--- ClientApp/KachnaOnline/src/app/users/user-profile/user-profile.component.ts
+++ ClientApp/KachnaOnline/src/app/users/user-profile/user-profile.component.ts
@@ -4,6 +4,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from "../../shared/services/authentication.service";
 import { NgForm } from "@angular/forms";
+import { prestigeText } from "../registration/registration.component";
 
 @Component({
   selector: 'app-user',
@@ -17,7 +18,7 @@ export class UserProfileComponent implements OnInit {
     public authenticationService: AuthenticationService,
   ) { }
 
-  gamificationConsentTooltipText:string = "Tímto souhlasíš s gamifikací v klubu U Kachničky."
+  gamificationConsentTooltipText:string = prestigeText;
   cardCodeTooltipText:string = "Pro přiřazení karty ke svému účtu řekni někomu za barem. Pípneš si kartou a barman ti dá kód, který sem napíšeš."
 
   ngOnInit(): void {
