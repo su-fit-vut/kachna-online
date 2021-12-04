2146f9a6d7b9772b12f8a9e41351f926772c0954
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2021-12-04 14:42:36 +0100

    Make back arrow usage more consistent

--- ClientApp/KachnaOnline/src/app/shared/components/back-arrow/back-arrow.component.ts
+++ ClientApp/KachnaOnline/src/app/shared/components/back-arrow/back-arrow.component.ts
@@ -1,7 +1,8 @@
 // back-arrow.component.ts
-// Author: František Nečas
+// Author: František Nečas, Ondřej Ondryáš
 
 import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-back-arrow',
@@ -9,11 +10,17 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./back-arrow.component.css']
 })
 export class BackArrowComponent implements OnInit {
-  @Input() targetRoute: string
+  @Input() targetRoute: string;
 
-  constructor() { }
+  @Input() headingSize: number = 2;
+  @Input() heading: string;
+
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
   }
 
+  back(): void {
+    this.location.back();
+  }
 }
