1413cb97cd459ebb3975210c37076ed304d68eaf
David Chocholatý <chocholaty.david@protonmail.com>
2021-12-16 20:30:31 +0100

    Rewrite user profile form logic
    Furthermore, allow sending card code to KIS.

--- ClientApp/KachnaOnline/src/app/users/user-profile/user-profile.component.ts
+++ ClientApp/KachnaOnline/src/app/users/user-profile/user-profile.component.ts
@@ -3,8 +3,12 @@
 
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from "../../shared/services/authentication.service";
-import { NgForm } from "@angular/forms";
+import { FormBuilder, FormControl, NgForm, Validators } from "@angular/forms";
 import { prestigeText } from "../registration/registration.component";
+import { UserDetail } from "../../models/users/user.model";
+import { ToastrService } from "ngx-toastr";
+import { KisLoggedInUserInformation } from "../../models/users/kis-logged-in-user-information.model";
+import { throwError } from "rxjs";
 
 @Component({
   selector: 'app-user',
@@ -16,19 +20,61 @@ export class UserProfileComponent implements OnInit {
 
   constructor(
     public authenticationService: AuthenticationService,
+    private fb: FormBuilder,
+    private toastrService: ToastrService,
   ) { }
 
   gamificationConsentTooltipText:string = prestigeText;
   cardCodeTooltipText:string = "Pro přiřazení karty ke svému účtu řekni někomu za barem. Pípneš si kartou a barman ti dá kód, který sem napíšeš."
 
+  form = this.fb.group({
+    name: [""],
+    email: [""],
+    nickname: ["", [Validators.maxLength(128)]],
+    gamificationConsent: [false],
+    cardCode: ["", [Validators.maxLength(5), Validators.pattern("[a-zA-Z]{5}")]],
+  })
   ngOnInit(): void {
-    this.authenticationService.updateLocalUserInformation();
+    this.authenticationService.getLocalUserInformationRequest().toPromise()
+      .then((userDetail: UserDetail) => {
+        this.form.controls.nickname.setValue(userDetail.nickname);
+      }).catch((err) => {
+      console.log(err);
+      this.toastrService.error("Nebylo možné načíst data o uživateli.", "Správa účtu");
+    });
+
+    this.authenticationService.getInformationAboutLoggedInUserFromKisRequest().toPromise()
+      .then((kisLoggedInUserInformation: KisLoggedInUserInformation) => {
+        this.form.controls.name.setValue(kisLoggedInUserInformation.name);
+        this.form.controls.email.setValue(kisLoggedInUserInformation.email);
+        //this.form.controls.nickname.setValue(kisLoggedInUserInformation.nickname);
+        this.form.controls.gamificationConsent.setValue(kisLoggedInUserInformation.gamification_consent);
+      }).catch(err => {
+        this.toastrService.error("Nebylo možné načíst data o uživateli.", "Správa účtu");
+        throwError(err);
+    });
   }
 
   ngOnDestroy(): void {
   }
 
-  onSaveChanges(form: NgForm) {
-    this.authenticationService.userInfoSaved();
+  onSaveChanges() {
+    //this.authenticationService.userInfoSaved();
+
+    let formVal = this.form.controls;
+    if (formVal.nickname.dirty) {
+      console.log(formVal.nickname)
+      this.authenticationService.updateNickname(formVal.nickname.value);
+    }
+
+    if (formVal.gamificationConsent.touched) {
+      this.authenticationService.changeGamificationConsent(formVal.gamificationConsent.value);
+    }
+
+    if (formVal.cardCode.touched) {
+      this.authenticationService.changeCardCode(formVal.cardCode.value);
+    }
+
+    this.form.markAsPristine();
   }
 }
