401f219beee81f7265bd784f1e26f212f62a859b
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2024-02-04 22:47:23 +0100

    Fix time locative pipe

--- ClientApp/KachnaOnline/src/app/shared/pipes/time-locative.pipe.ts
+++ ClientApp/KachnaOnline/src/app/shared/pipes/time-locative.pipe.ts
@@ -7,7 +7,7 @@ export class TimeLocativePipe implements PipeTransform {
 
   transform(value: Date): string {
     let hours = value.getHours();
-    let isLong = hours in [2, 3, 4, 12, 14, 20, 21, 22, 23];
+    let isLong = [2, 3, 4, 12, 14, 20, 21, 22, 23].includes(hours);
     return (isLong ? "ve " : "v ") + value.toLocaleTimeString("cs-cz", {hour: "numeric", minute: "numeric"});
   }
 
